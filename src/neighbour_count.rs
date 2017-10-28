a426c7df7a94c73a628cb64354db4599b68c0a6c
Stephen Sherratt <sfsherratt@gmail.com>
2017-10-28 20:42:30 +0100

    Initial commit

---
+++ src/neighbour_count.rs
@@ -0,0 +1,56 @@
+use direction::{Direction, DirectionBitmap, NUM_DIRECTIONS};
+
+const NUM_NEIGHBOURS: usize = NUM_DIRECTIONS;
+
+/// Table for managing counts associated with directions.
+/// This is used by the generated spatial hash as
+/// storage for the `neighbour_count` aggregate.
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+pub struct NeighbourCount {
+    neighbours: [u8; NUM_NEIGHBOURS],
+}
+
+impl NeighbourCount {
+    pub fn new() -> Self {
+        NeighbourCount {
+            neighbours: [0; NUM_NEIGHBOURS],
+        }
+    }
+
+    pub fn inc(&mut self, direction: Direction) {
+        self.neighbours[direction as usize] += 1;
+    }
+
+    pub fn dec(&mut self, direction: Direction) {
+        self.neighbours[direction as usize] -= 1;
+    }
+
+    pub fn get(&self, direction: Direction) -> u8 {
+        self.neighbours[direction as usize]
+    }
+
+    pub fn has(&self, direction: Direction) -> bool {
+        self.neighbours[direction as usize] != 0
+    }
+
+    pub fn bitmap_raw(&self) -> u8 {
+        (((self.neighbours[0] != 0) as u8) << 0) |
+        (((self.neighbours[1] != 0) as u8) << 1) |
+        (((self.neighbours[2] != 0) as u8) << 2) |
+        (((self.neighbours[3] != 0) as u8) << 3) |
+        (((self.neighbours[4] != 0) as u8) << 4) |
+        (((self.neighbours[5] != 0) as u8) << 5) |
+        (((self.neighbours[6] != 0) as u8) << 6) |
+        (((self.neighbours[7] != 0) as u8) << 7)
+    }
+
+    pub fn bitmap(&self) -> DirectionBitmap {
+        DirectionBitmap::new(self.bitmap_raw())
+    }
+}
+
+impl Default for NeighbourCount {
+    fn default() -> Self {
+        Self::new()
+    }
+}
